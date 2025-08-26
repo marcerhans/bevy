026e93b2e249d9ff18fac588ecf69a25a62fdaab
marcerhans <marcerhans@gmail.com>
2025-08-26 21:59:09 +0200

    ...

--- package/project/mahjong/examples/observer.rs
+++ package/project/mahjong/examples/observer.rs
@@ -15,7 +15,7 @@ fn on_startup(mut commands: Commands) {
         .spawn(Name::new("1"))
         .observe(on_exploded) //
         .observe(|trigger: Trigger<Exploded>| {
-            info!("WHAT? {:?}", trigger.target());
+            info!("WHAT1? {:?}", trigger.target());
         })
         .id();
     info!("Spawned: {:?}", e1);
@@ -29,28 +29,23 @@ fn on_startup(mut commands: Commands) {
         .spawn(Name::new("3"))
         .observe(on_exploded) //
         .observe(|trigger: Trigger<Exploded>| {
-            info!("WHAT? {:?}", trigger.target());
+            info!("WHAT3? {:?}", trigger.target());
         })
         .id();
     info!("Spawned: {:?}", e3);
 
     info!("Before trigger");
-    // commands.trigger(Exploded);
+    commands.trigger(Exploded);
     commands.trigger_targets(Exploded, e1);
-    // commands.trigger_targets(Exploded, e2);
+    commands.trigger_targets(Exploded, e2);
     commands.trigger_targets(Exploded, e3);
     info!("After trigger");
 }
 
 fn on_exploded(
     trigger: Trigger<Exploded>,
-    name: Query<&Name>,
     mut counter: Local<u32>,
 ) {
-    info!("on_exploded! count: {:?}", *counter);
+    info!("on_exploded {:?}|{:?}", *counter, trigger.target());
     *counter += 1;
-
-    if let Ok(name) = name.get(trigger.target()) {
-        info!("on_exploded {:?}|{:?}", name, trigger.target());
-    }
 }
