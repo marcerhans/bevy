8770c7d2c9a70ccf44452be23eea7ef2bdf4568b
marcerhans <marcerhans@gmail.com>
2025-09-05 22:13:02 +0200

    ...

--- package/project/mahjong/src/plugin/scene/a/mod.rs
+++ package/project/mahjong/src/plugin/scene/a/mod.rs
@@ -1,5 +1,5 @@
-use bevy::prelude::*;
 use crate::plugin::scene::Startup;
+use bevy::prelude::*;
 
 pub struct Plugin;
 
@@ -8,9 +8,10 @@ impl bevy::prelude::Plugin for Plugin {
         &self,
         app: &mut App,
     ) {
+        app.add_sub_state::<Menu>()
+            .add_systems(OnEnter(Startup::Running), on_enter);
     }
 }
-
 
 #[derive(SubStates, Hash, Eq, PartialEq, Clone, Debug, Default)]
 #[source(Startup = Startup::Running)]
@@ -21,4 +22,33 @@ pub enum Menu {
     Play,
     Settings,
     Quit,
+}
+
+#[derive(Component)]
+struct Marker;
+
+fn on_enter(mut commands: Commands) {
+    let font = (
+        TextFont { ..default() },
+        TextColor(Color::srgb(0.9, 0.9, 0.9)),
+    );
+
+    commands.spawn((
+        Marker,
+        StateScoped(Menu::Root),
+        Node {
+            width: Val::Percent(100.0),
+            height: Val::Percent(100.0),
+            justify_content: JustifyContent::Center,
+            align_items: AlignItems::Center,
+            flex_direction: FlexDirection::Column,
+            ..default()
+        },
+        BackgroundColor(Color::srgb(0.2, 0.2, 0.2)),
+        children![
+            (Text::new("Play"), font.clone()),
+            (Text::new("Settings"), font.clone()),
+            (Text::new("Quit"), font.clone()),
+        ],
+    ));
 }
