bd2a655836fa64eb8b1ed93398369a40797f24c5
marcerhans <marcerhans@gmail.com>
2025-11-26 22:38:12 +0100

    Reactive greeter screen.

--- package/project/mahjong/src/plugin/scene/greeter.rs
+++ package/project/mahjong/src/plugin/scene/greeter.rs
@@ -12,7 +12,7 @@ impl bevy::prelude::Plugin for Plugin {
     ) {
         app.init_resource::<TimerRes>()
             .add_systems(OnEnter(Startup::Greeter), on_enter)
-            .add_systems(Update, on_update);
+            .add_systems(Update, on_update.run_if(in_state(Startup::Greeter)));
     }
 }
 
@@ -28,6 +28,9 @@ impl Default for TimerRes {
         }
     }
 }
+
+#[derive(Component)]
+struct BackgroundSprite;
 
 #[derive(Component)]
 struct Marker;
@@ -48,6 +51,7 @@ fn on_enter(
     commands.spawn((
         DespawnOnExit(Startup::Greeter),
         Marker,
+        BackgroundSprite,
         Sprite {
             custom_size: Some(Vec2::new(projection.area.width(), projection.area.height())),
             ..Sprite::from_image(greeter_asset)
@@ -62,10 +66,25 @@ fn on_update(
     time: Res<Time>,
     mut timer: ResMut<TimerRes>,
     mut next_state: ResMut<NextState<Startup>>,
+    mut transform: Query<&mut Sprite, With<BackgroundSprite>>,
+    projection: Query<&Projection, With<Camera>>,
 ) {
     timer.inner.tick(time.delta());
 
     if timer.inner.just_finished() {
         next_state.set(Startup::MainMenu);
     }
+
+    let Some(Projection::Orthographic(projection)) = projection.iter().next() else {
+        panic!();
+    };
+
+    let Some(mut sprite) = transform.iter_mut().next() else {
+        return;
+    };
+
+    sprite.custom_size = Some(Vec2 {
+        x: projection.area.width(),
+        y: projection.area.height(),
+    });
 }
