8555f99071583909fedaa4224b87c91384655ae9
marcerhans <marcerhans@gmail.com>
2026-01-01 15:08:31 +0100

    ...

--- package/project/mahjong/src/plugin/scene/in_game/mod.rs
+++ package/project/mahjong/src/plugin/scene/in_game/mod.rs
@@ -36,6 +36,8 @@ mod marker {
 }
 
 mod tile {
+    use std::marker::PhantomData;
+
     use bevy::prelude::*;
 
     pub mod asset {
@@ -61,6 +63,119 @@ mod tile {
 
     #[derive(Component, Deref, DerefMut)]
     pub struct Position(UVec3);
+
+    impl Position {
+        fn generator<T>() -> PositionGenerator<T> {
+            PositionGenerator::<T> {
+                counter: 0,
+                _type: PhantomData,
+            }
+        }
+    }
+
+    pub struct Turtle;
+
+    pub struct PositionGenerator<T> {
+        counter: u32,
+        _type: PhantomData<T>,
+    }
+
+    impl Iterator for PositionGenerator<Turtle> {
+        type Item = Position;
+
+        fn next(&mut self) -> Option<Self::Item> {
+            let layer;
+            let row;
+
+            match current {
+                ..87 => {
+                    layer = 0;
+                    match current {
+                        0..12 => row = 0,
+                        12..20 => row = 1,
+                        20..30 => row = 2,
+                        30..42 => row = 3,
+                        42..54 => row = 4,
+                        54..64 => row = 5,
+                        64..72 => row = 6,
+                        72..84 => row = 7,
+                        84..87 => row = 8,
+                        _ => unreachable!(),
+                    };
+                },
+                87..123 => {
+                    layer = 1;
+                    row = (current - 87) / 6 + 1;
+                },
+                123..139 => {
+                    layer = 2;
+                    row = (current - 123) / 4 + 2;
+                },
+                139..143 => {
+                    layer = 3;
+                    row = (current - 139) / 2 + 3;
+                },
+                143 => {
+                    // Special case. Just return value immediately.
+                    let row = 3.5 * 2.0;
+                    let column = 5.5 * 2.0;
+                    let layer = 4.0 * 2.0;
+                    let local_position = Position(UVec3::new(column as u32, row as u32, layer as u32));
+                    return Some(local_position);
+                },
+                _ => return None,
+            }
+
+            // let column = match layer {
+            //     0 => {
+            //         match row {
+            //             0 => 0 + current - 0,
+            //             1 => 2 + current - 12,
+            //             2 => 1 + current - 20,
+            //             3 => 0 + current - 30,
+            //             4 => 0 + current - 42,
+            //             5 => 1 + current - 54,
+            //             6 => 2 + current - 64,
+            //             7 => 0 + current - 72,
+            //             8 => match current - 84 {
+            //                 // Last 3 are special cases. Do not follow a pattern.
+            //                 0 => {
+            //                     let row = 3.5;
+            //                     let column = -1.0;
+            //                     let local_position = Vec3::new(column, row, layer as f32)
+            //                         * self.tile_size.extend(1.0);
+            //                     return Some(self.local2global(&local_position) + offsets);
+            //                 },
+            //                 1 => {
+            //                     let row = 3.5;
+            //                     let column = 12.0;
+            //                     let local_position = Vec3::new(column, row, layer as f32)
+            //                         * self.tile_size.extend(1.0);
+            //                     return Some(self.local2global(&local_position) + offsets);
+            //                 },
+            //                 2 => {
+            //                     let row = 3.5;
+            //                     let column = 13.0;
+            //                     let local_position = Vec3::new(column, row, layer as f32)
+            //                         * self.tile_size.extend(1.0);
+            //                     return Some(self.local2global(&local_position) + offsets);
+            //                 },
+            //                 _ => unreachable!(),
+            //             },
+            //             _ => unreachable!(),
+            //         }
+            //     },
+            //     1 => 3 + ((current - 87) % 6),
+            //     2 => 4 + ((current - 123) % 4),
+            //     3 => 5 + ((current - 139) % 2),
+            //     _ => unreachable!(),
+            // };
+
+            // let local_position =
+            //     Vec3::new(column as f32, row as f32, layer as f32) * self.tile_size.extend(1.0);
+            // Some(self.local2global(&local_position) + offsets)
+        }
+    }
 
     #[derive(Component, Deref, DerefMut)]
     pub struct Variant(u32);
